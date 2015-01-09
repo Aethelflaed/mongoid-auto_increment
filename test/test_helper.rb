6e1a571d7f2a348a21a11d9daa94ac38369d81cf
Geoffroy Planquart <geoffroy@planquart.fr>
2015-01-09 19:06:38 +0100

    base for tests

---
+++ test/test_helper.rb
@@ -0,0 +1,22 @@
+require 'bundler/setup'
+require 'simplecov'
+SimpleCov.configure do
+  add_filter '/test/'
+end
+SimpleCov.start if ENV['COVERAGE']
+
+require 'minitest/autorun'
+require 'mongoid'
+
+require File.expand_path("../../lib/mongoid-auto_increment", __FILE__)
+
+Mongoid.load!("#{File.dirname(__FILE__)}/mongoid.yml", "test")
+
+Dir["#{File.dirname(__FILE__)}/models/*.rb"].each { |f| require f }
+
+class BaseTest < ActiveSupport::TestCase
+  def teardown
+    Mongoid::Sessions.default.use('mongoid_auto_increment_test').drop
+  end
+end
+
